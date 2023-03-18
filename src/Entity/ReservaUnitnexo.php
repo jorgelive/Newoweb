d971bae6d7fb3833ae4ea8f45922e3ee5bd0d48e
Jorge Luis Gomez <jorge@live.com.pe>
2023-03-18 10:23:23 -0500

    Correccion

--- src/Entity/ReservaUnitnexo.php
+++ src/Entity/ReservaUnitnexo.php
@@ -2,7 +2,7 @@
 
 namespace App\Entity;
 
-use Doctrine\Common\Collections\Collection
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Validator\Constraints as Assert;
 use Gedmo\Mapping\Annotation as Gedmo;
