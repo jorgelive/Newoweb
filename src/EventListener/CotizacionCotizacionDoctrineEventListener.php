b2f645c70b53f9436119e56f24989de64391062f
Jorge Gomez <jorge@openperu.pe>
2023-05-10 22:01:53 -0500

    Verificación de Cotización al guardar

--- src/EventListener/CotizacionCotizacionDoctrineEventListener.php
+++ src/EventListener/CotizacionCotizacionDoctrineEventListener.php
@@ -2,10 +2,24 @@
 namespace App\EventListener;
 
 use App\Entity\CotizacionCotizacion;
+use App\Service\CotizacionResumen;
 use Doctrine\ORM\Event\LifecycleEventArgs;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class CotizacionCotizacionDoctrineEventListener
 {
+
+    private CotizacionResumen $cotizacionResumen;
+
+    private RequestStack $requestStack;
+
+    public function __construct(CotizacionResumen $cotizacionResumen, RequestStack $requestStack)
+    {
+        $this->cotizacionResumen = $cotizacionResumen;
+
+        $this->requestStack = $requestStack;
+
+    }
 
     public function prePersist(LifecycleEventArgs $args)
     {
@@ -15,6 +29,27 @@ class CotizacionCotizacionDoctrineEventListener
                 $entity->setToken(mt_rand());
             }
 
+        }
+    }
+
+    public function postPersist(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+        if($entity instanceof CotizacionCotizacion){
+            if(!$this->cotizacionResumen->procesar($entity->getId())){
+                $this->requestStack->getSession()->getFlashBag()->add('error', $this->cotizacionResumen->getMensaje());
+            }
+        }
+    }
+
+    public function postUpdate(LifecycleEventArgs $args)
+    {
+        $entity = $args->getEntity();
+
+        if($entity instanceof CotizacionCotizacion){
+            if(!$this->cotizacionResumen->procesar($entity->getId())){
+                $this->requestStack->getSession()->getFlashBag()->add('error', $this->cotizacionResumen->getMensaje());
+            }
         }
     }
 }
