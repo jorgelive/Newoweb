748690264c5603a1a32194a77675a74b36868438
Jorge Gomez <jorge@openperu.pe>
2025-04-13 20:16:40 -0500

    Correccion

--- src/Block/TareasBlockService.php
+++ src/Block/TareasBlockService.php
@@ -33,7 +33,7 @@ class TareasBlockService extends AbstractBlockService
         $resolver->setDefaults([
             'url' => false,
             'title' => 'Tareas',
-            'template' => 'Block/Tareas.html.twig',
+            'template' => 'block/Tareas.html.twig',
             'class' => 'Tareas',
             'icon' => false,
             'translation_domain' => 'messages'
